75d80c2bb7f3c8274996232f20a9ae33c8ef4579
kimolate <kimolatelei@outlook.com>
2015-09-13 20:01:33 +0800

    V0.1
    实现smtp非加密发送邮件
    实现从163发送qq邮箱(发送地址必须163邮箱,发送名必须163邮箱名)

---
+++ smtp邮箱/smtp邮箱/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace smtp邮箱
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public smtp邮箱.Send mailSend = new smtp邮箱 .Send ();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mailSend.sendEmail = textBox1.Text;
+            mailSend.getEmail = textBox2.Text;
+            mailSend.emailTitle = textBox3.Text;
+            mailSend.emailContent = textBox4.Text;
+            mailSend.SendStmpEmail();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
---
+++ smtp邮箱/smtp邮箱/Send.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net .Mail ;
+using System.Windows .Forms ;
+
+namespace smtp邮箱
+{
+    public class Send
+    {
+
+        public string sendEmail { get; set; }
+        public string getEmail { get; set; }
+        public string emailTitle { get; set; }
+        public string emailContent { get; set; }
+
+
+        public void SendStmpEmail()
+        {
+            MailMessage msg = new MailMessage();
+            msg.To.Add(getEmail);
+            msg.From = new MailAddress(sendEmail, "ki", System.Text.Encoding.UTF8);
+            msg.Subject = emailTitle;
+            msg.SubjectEncoding = System.Text.Encoding.UTF8;
+            msg.Body = emailContent;
+            msg.BodyEncoding = System.Text.Encoding.UTF8;
+            msg.IsBodyHtml = false;
+            msg.Priority = MailPriority.High;
+
+
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.163.com";
+            client.Credentials = new System.Net.NetworkCredential(sendEmail, "leimingkai130262");
+
+
+            try
+            {
+                client.Send(msg);
+                MessageBox.Show("发送成功！！");
+
+            }
+            catch (SmtpException ex)
+            {
+                MessageBox.Show(ex.Message ,"发送失败！！");
+            }
+        }
+
+    }
+}
